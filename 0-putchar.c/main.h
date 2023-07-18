f1ff64c4110528d8d931b349fdc15350ca12d392
Motunrayo <Moturayoomijie123@gmail.com>
2023-07-18 08:27:01 -0700

    0-putchsr.c

---
+++ 0-putchar.c/main.h
@@ -0,0 +1,19 @@
+#ifndef main_h
+#define main_h
+
+int_putchar(char):
+void print_alphabet(void):
+void print _alphabet_x10(void):
+int_islower(int c):
+int_isalpha(int c):
+int print_sign(int n):
+int _abs(int):
+int print _last_digit(int):
+void jack_bauer(void):
+void times_table(void):
+int add(int,int):
+void print_to_98(int n):
+void print_times_table(int n):
+
+
+#endif
