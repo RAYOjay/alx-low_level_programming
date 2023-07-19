3bee15a7a201a6c8f8295cde7b58c9e2079d0aac
Motunrayo <Moturayoomijie123@gmail.com>
2023-07-19 07:29:55 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 
@@ -8,3 +8,4 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif /* MAIN_H */
+
