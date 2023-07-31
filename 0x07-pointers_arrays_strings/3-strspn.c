69d703f3cfc1d1c4824d5fd8675c4dc433caefa2
Motunrayo <Moturayoomijie123@gmail.com>
2023-07-31 12:29:04 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  *   * @s: input
  *    * @accept: input
  *     * Return: Always 0 (Success)
- *      */
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 		unsigned int n = 0;
@@ -26,4 +26,3 @@ unsigned int _strspn(char *s, char *accept)
 												}
 					return (n);
 }
-
