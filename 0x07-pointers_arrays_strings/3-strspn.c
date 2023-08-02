dcdd8934cc66bda1b0f51017e499ef2660ba36cb
Motunrayo <Motunrayoomijie123@gmail.com>
2023-08-02 03:11:39 +0100

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,45 +4,26 @@
  *   * @s: input
  *    * @accept: input
  *     * Return: Always 0 (Success)
- */
+ *      */
 unsigned int _strspn(char *s, char *accept)
 {
-<<<<<<< HEAD
-	        unsigned int n = 0;
-		        int r;
-			        while (*s)
-					        {
-							                for (r = 0; accept[r]; r++)
-										                {
-													                        if (*s == accept[r])
-																	                        {
-																					                                n++;
-																									                                break;
-																													                        }
-																                        else if (accept[r + 1] == '\0')
-																				                                return (n);
-																			                }
-									                s++;
-											        }
-				        return (n);
-=======
-	unsigned int n = 0;
-	int r;
+		unsigned int n = 0;
+			int r;
 
-	while (*s)
-	{
-		for (r = 0; accept[r]; r++)
-		{	
-																		if (*s == accept[r])
-																		{
-																				n++;
-																				break;
-																																	}
-																		else if (accept[r + 1] == '\0')
-																			return (n);
-																	}
-		s++;
-	}
-	return (n);
->>>>>>> e68547846f5405ba1f95615751725792389c91bb
+				while (*s)
+						{
+									for (r = 0; accept[r]; r++)
+												{
+																if (*s == accept[r])
+																				{
+																									n++;
+																													break;
+																																}
+																			else if (accept[r + 1] == '\0')
+																								return (n);
+																					}
+											s++;
+												}
+					return (n);
 }
+
