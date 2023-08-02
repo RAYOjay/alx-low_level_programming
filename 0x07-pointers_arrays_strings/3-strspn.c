e68547846f5405ba1f95615751725792389c91bb
Motunrayo <Moturayoomijie123@gmail.com>
2023-08-01 17:07:47 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,17 +12,16 @@ unsigned int _strspn(char *s, char *accept)
 
 	while (*s)
 	{
-		for (r = 0; accept[r]; r++);
-		{
-
-			if (*s == accept[r]);
-			{															n++;
-			    break;
-			}
-
-			else if (accept[r + 1] == '\0');
-				return (n);
-		}
+		for (r = 0; accept[r]; r++)
+		{	
+																		if (*s == accept[r])
+																		{
+																				n++;
+																				break;
+																																	}
+																		else if (accept[r + 1] == '\0')
+																			return (n);
+																	}
 		s++;
 	}
 	return (n);
