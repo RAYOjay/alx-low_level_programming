7190e9064f071fd0334397acbe91d74283fc10c0
Motunrayo <Motunrayoomijie123@gmail.com>
2023-08-03 08:49:19 +0100

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,7 +10,7 @@ int _strlen_recursion(char *s);
  *  Return: 1 if it is, 0 it's not
  */
 int is_palindrome(char *s)
-{ 
+{
 	if (*s == 0)
 		return (1);
 	return (check_pal(s, 0, _strlen_recursion(s)));
